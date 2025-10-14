7a0a0ebffa53fd86ec9e1d9c208c455611988688
fabianasoti <fsotillocuevas@gmail.com>
2025-10-14 19:54:01 +0200

    Update simulacro-parcial001.py

--- PROGRAMACIÓN/Simulacros/simulacro-parcial001.py
+++ PROGRAMACIÓN/Simulacros/simulacro-parcial001.py
@@ -1,64 +1,75 @@
+'''
+Generador de facturas
+v0.1 2025 Fabiana Victoria Sotillo
+Se genera un ticket calculando posibles descuentos e IVA
+'''
 ###### Se establecen variables
 cliente_nombre = input("Introduce tu nombre: ")
+# Entrada edad convertida a entero
+edad = int(input("Introduce tu edad: ")) # Convierto edad a entero
 
-edad = input("Introduce tu edad: ") 
-edad_entero = int(edad) # Convierto edad a entero
-
-#   Reglas de negocio
-if edad_entero < 18:   # Si edad < 18: no emitir factura
+# Reglas de negocio 
+if edad <= 0:
+	print("Edad invalida")
+elif edad < 18: # Si edad < 18: no emitir factura
     print("Es menor de 18 años, no se puede emitir factura.")
     exit()
 else:    # Si es mayor de 18, es adulto: se emite factura
     print("Tiene la edad apta para generar su factura.")
 
-base_imponible = (input("Introduce la base imponible de tu factura: ")) 
-base_decimal = float(base_imponible)    # Convierto base en decimal
+# Entrada base convertida en decimal
+base_imponible = float(input("Introduce la base imponible de tu factura: "))
 
+if base_imponible <= 0: # Me aseguro de que es un número positivo
+    print("Error: Base imponible inválida")
 
 # Declaro constantes
 IVA = 0.21 # Es el 21%
     
     
-# Cálculos requeridos y reglas de negocio
-
-importe_descuento = 0
-descuento_decimal = float(importe_descuento)     # Convierto descuento en decimal
+# Definición del descuento
+porcentaje_descuento = 0
+descuento = 0
 # Descuento según base_imponible
-if base_decimal < 100: # < 100 €: 0%
-    descuento_decimal = 0
-    base_tras_descuento = base_decimal - descuento_decimal
-    print("Descuento %", descuento_decimal)
-    print("Descuento en euros:", descuento_decimal)
+if base_imponible < 100: # < 100 €: 0%
+    porcentaje_descuento = 0
+    descuento = 0
     
-elif base_decimal <= 100 and base_imponible >= 199.99: # 100–199.99 €: 5%
-    descuento_decimal = 0.5
-    base_tras_descuento = base_decimal - descuento_decimal
-    print("Descuento %", descuento_decimal)
-    print("Descuento en euros:", descuento_decimal)
+elif base_imponible <= 100 and base_imponible >= 199.99: # 100–199.99 €: 5%
+    porcentaje_descuento = 5
+    descuento = 0.05
+    
 else:   # ≥ 200 €: 10%
-    descuento_decimal = 0.10
-    base_tras_descuento = base_decimal - descuento_decimal
-    print("Descuento %", descuento_decimal)
-    print("Descuento en euros:", descuento_decimal)
+    porcentaje_descuento = 10
+    descuento = 0.1
 
 # Desglose: base, % descuento, € descuento, base tras descuento, IVA, TOTAL
 
-base_tras_descuento = base_decimal - descuento_decimal
-print("Tu base con el descuento es:", base_tras_descuento)
-
-importe_iva = base_tras_descuento * IVA
-print("Importe IVA 21%:", importe_iva)
-
+# Cálculos requeridos
+importe_descuento = base_imponible * descuento # Lo que se descuenta en euros
+base_tras_descuento = base_imponible - importe_descuento # A pagar sin iva
+importe_iva = base_tras_descuento * IVA # el porcentaje que corresponde de IVA según base
 total_factura = base_tras_descuento + importe_iva
-print("Total:", total_factura)
     
     
 # Ticket
 
 print("______________________________________________")
-print("  Factura con descuento y calculadora de IVA  ")
+print("|           Generador de facturas            |")
+print("|          Fabiana Victoria Sotillo          |")
+print("|                 v0.1 2025                  |")
+print("|____________________________________________|")
+print("Nombre: ", cliente_nombre)
+print("Edad: ", edad)
+print("----------------------------------------------")
+print("Base imponible:",base_imponible, "€")
+print("Porcentaje de descuento:", porcentaje_descuento, "%")
+print("Importe descuento:", importe_descuento, "€")
+print("Base tras descuento:", base_tras_descuento, "€")
+print("IVA (21%):", importe_iva, "€")
+print("                              ----------------")
+print("Total:", total_factura, "€")
 print("______________________________________________")
-
 
 
 
