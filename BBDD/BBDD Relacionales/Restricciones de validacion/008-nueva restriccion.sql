a2157502e60e2141e2cf115fce8f45649922bccd
fabianasoti <fsotillocuevas@gmail.com>
2025-10-15 14:22:31 +0200

    Update 008-nueva restriccion.sql

--- BBDD/BBDD Relacionales/Restricciones de validacion/008-nueva restriccion.sql
+++ BBDD/BBDD Relacionales/Restricciones de validacion/008-nueva restriccion.sql
@@ -1,67 +1,33 @@
-DESCRIBE clientes;
-+---------------+--------------+------+-----+---------+----------------+
-| Field         | Type         | Null | Key | Default | Extra          |
-+---------------+--------------+------+-----+---------+----------------+
-| identificador | int          | NO   | PRI | NULL    | auto_increment |
-| dni           | varchar(9)   | YES  |     | NULL    |                |
-| nombre        | varchar(50)  | YES  |     | NULL    |                |
-| apellidos     | varchar(255) | YES  |     | NULL    |                |
-| email         | varchar(100) | YES  |     | NULL    |                |
-+---------------+--------------+------+-----+---------+----------------+
-
 ALTER TABLE clientes
-ADD COLUMN direccion VARCHAR(255);
-
-Query OK, 0 rows affected (0,04 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
-DESCRIBE clientes; 
-+---------------+--------------+------+-----+---------+----------------+
-| Field         | Type         | Null | Key | Default | Extra          |
-+---------------+--------------+------+-----+---------+----------------+
-| identificador | int          | NO   | PRI | NULL    | auto_increment |
-| dni           | varchar(9)   | YES  |     | NULL    |                |
-| nombre        | varchar(50)  | YES  |     | NULL    |                |
-| apellidos     | varchar(255) | YES  |     | NULL    |                |
-| email         | varchar(100) | YES  |     | NULL    |                |
-| direccion     | varchar(255) | YES  |     | NULL    |                |
-+---------------+--------------+------+-----+---------+----------------+
-6 rows in set (0,00 sec)
-
-
-ALTER TABLE clientes
-DROP COLUMN direccion;
-
-DESCRIBE clientes;
-+---------------+--------------+------+-----+---------+----------------+
-| Field         | Type         | Null | Key | Default | Extra          |
-+---------------+--------------+------+-----+---------+----------------+
-| identificador | int          | NO   | PRI | NULL    | auto_increment |
-| dni           | varchar(9)   | YES  |     | NULL    |                |
-| nombre        | varchar(50)  | YES  |     | NULL    |                |
-| apellidos     | varchar(255) | YES  |     | NULL    |                |
-| email         | varchar(100) | YES  |     | NULL    |                |
-+---------------+--------------+------+-----+---------+----------------+
-
-
-ALTER TABLE clientes
-RENAME COLUMN dni TO dninie;
-
-ERROR 3959 (HY000): Check constraint 'comprobar_dni_nie_letra' uses column 'dni', hence column cannot be dropped or renamed.
-
-ALTER TABLE clientes
-DROP CONSTRAINT comprobar_dni_nie_letra;
-Query OK, 0 rows affected (0,03 sec)
-
-DESCRIBE clientes;
-+---------------+--------------+------+-----+---------+----------------+
-| Field         | Type         | Null | Key | Default | Extra          |
-+---------------+--------------+------+-----+---------+----------------+
-| identificador | int          | NO   | PRI | NULL    | auto_increment |
-| dni           | varchar(9)   | YES  |     | NULL    |                |
-| nombre        | varchar(50)  | YES  |     | NULL    |                |
-| apellidos     | varchar(255) | YES  |     | NULL    |                |
-| email         | varchar(100) | YES  |     | NULL    |                |
-+---------------+--------------+------+-----+---------+----------------+
-5 rows in set (0,00 sec)
+  ADD CONSTRAINT comprobar_dni_nie_letra
+  CHECK (
+    (
+      -- DNI: 8 dígitos + letra
+      dninie REGEXP '^[0-9]{8}[A-Za-z]$'
+      AND
+      UPPER(SUBSTRING(dninie, 9, 1)) =
+      SUBSTRING('TRWAGMYFPDXBNJZSQVHLCKE',
+                (CAST(SUBSTRING(dninie, 1, 8) AS UNSIGNED) MOD 23) + 1,
+                1)
+    )
+    OR
+    (
+      -- NIE: X/Y/Z + 7 dígitos + letra
+      dninie REGEXP '^[XYZxyz][0-9]{7}[A-Za-z]$'
+      AND
+      UPPER(SUBSTRING(dninie, 9, 1)) =
+      SUBSTRING('TRWAGMYFPDXBNJZSQVHLCKE',
+                (
+                  CAST(CONCAT(
+                        CASE UPPER(SUBSTRING(dninie, 1, 1))
+                          WHEN 'X' THEN '0'
+                          WHEN 'Y' THEN '1'
+                          WHEN 'Z' THEN '2'
+                        END,
+                        SUBSTRING(dninie, 2, 7)
+                  ) AS UNSIGNED) MOD 23
+                ) + 1,
+                1)
+    )
+  );
 
