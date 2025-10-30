049b93c90b664135809d4560f357c5b9febf304d
fabianasoti <fsotillocuevas@gmail.com>
2025-10-30 13:52:56 +0100

    -

--- PROGRAMACIÓN/002-Utilización de objetos/007-Destrucción de objetos y liberación de memoria/010-propiedades.py
+++ PROGRAMACIÓN/002-Utilización de objetos/007-Destrucción de objetos y liberación de memoria/010-propiedades.py
@@ -1,6 +1,6 @@
 ''' 
     Calculadora de cuadras
-    v0.1 (c) 2025 Jose Vicente Carratalá
+    v0.1 Fabiana Victoria Sotillo
     Programa que calcula número de cuadras a partir de los caballos
 '''
 
