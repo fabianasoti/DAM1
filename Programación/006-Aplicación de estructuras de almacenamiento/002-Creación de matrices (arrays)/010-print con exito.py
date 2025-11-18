f057d66b8bd725f3d89340452038692b484b7fc7
fabianasoti <fsotillocuevas@gmail.com>
2025-11-18 12:39:31 +0100

    1

---
+++ Programación/006-Aplicación de estructuras de almacenamiento/002-Creación de matrices (arrays)/010-print con exito.py
@@ -0,0 +1,22 @@
+import pickle
+
+menu =[]
+
+while True:
+	print("Opciones:")
+	print("1.- Introducir nueva comida en el menú")
+	print("2.- Listar nueva comida en el menú")
+	print("3.- Guardar archivo")
+	opcion = int(input("Selecciona una opción:"))
+	if opcion == 1:
+		comida = input("Introduce el nombre de la comida: ")
+		menu.append(comida)
+	elif opcion == 2:
+		print("Tu comida hasta el momento es: ")
+		for elemento in menu:
+			print(elemento)
+	elif opcion == 3:
+		archivo = open("datos.bin","wb")	# Write Binary
+		pickle.dump(menu,archivo)	# Primero lo que se guarda, luego en donde
+		archivo.close()
+		print("Se ha guardado con éxito ✅")
