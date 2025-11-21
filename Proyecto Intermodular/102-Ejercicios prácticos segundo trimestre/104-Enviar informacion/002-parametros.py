694c3c989eb3664a4077986d297403447580aa0d
fabianasoti <fsotillocuevas@gmail.com>
2025-11-21 14:18:42 +0100

    1

---
+++ Proyecto Intermodular/102-Ejercicios prácticos segundo trimestre/104-Enviar informacion/002-parametros.py
@@ -0,0 +1,15 @@
+from flask import Flask, request	# tomo parámetros de la url
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+	nombre = request.args.get("nombre")
+	print(nombre)
+	return "Mira en la consola si ha pasado algo"
+
+if __name__ == "__main__":
+	app.run(debug=True)
+	
+# http://127.0.0.1:5000/?nombre=Fabiana
+# %20 = espacio (en url)
