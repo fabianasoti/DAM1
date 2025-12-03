213927910d592dba3911fe60c530d4d863542d86
fabianasoti <fsotillocuevas@gmail.com>
2025-12-03 09:41:24 +0100

    1

---
+++ Programación/010-Programación en el lado del servidor/002-get y post/004-dos parametros get.php
@@ -0,0 +1,10 @@
+<?php
+	echo $_GET['nombre'];
+	echo "<br>";
+	echo $_GET['apellidos'];
+?>
+
+http://localhost/DAM1/Programaci%C3%B3n/010-Programaci%C3%B3n%20en%20el%20lado%20del%20servidor/002-get%20y%20post/004-dos%20parametros%20get.php?nombre=Fabiana%20Victoria&apellidos=Sotillo%20Cuevas
+
+// %20 = espacios
+// las web con GET son más inseguras porque se pueden ver los parámetros y se pueden editar?
