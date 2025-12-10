2eff1cf3d5f8d6df06b41174db6af8dbe3f52c3a
fabianasoti <fsotillocuevas@gmail.com>
2025-12-10 14:09:49 +0100

    1

---
+++ Base de Datos/008-Proyectos/002-CRUD/001-Create/001-crear.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<title>Fabiana Victoria Sotillo</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<form action="002-procesa.php" method="POST">
+			<input type="text" name="nombre" placeholder="nombre">
+			<input type="text" name="puesto" placeholder="puesto">
+			<input type="text" name="salario" placeholder="salario">
+			<input type="text" name="fecha_contratacion" placeholder="fecha_contratacion">
+			<input type="text" name="departamento" placeholder="departamento">
+			<input type="submit">
+		</form>
+	</body>
+</html>
