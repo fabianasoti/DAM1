3ce38129043e66111021a71311bffd12467bd87e
fabianasoti <fsotillocuevas@gmail.com>
2025-12-17 09:44:53 +0100

    1

---
+++ Programación/010-Programación en el lado del servidor/011-Supercontrolador/004-Capricho de usabilidad/back/controladores/poblar_menu.php
@@ -0,0 +1,24 @@
+<?php
+	// Ahora lo que quiero es un listado de las tablas en la base de datos
+	$resultado = $conexion->query("
+		SHOW TABLES;
+	");
+	while ($fila = $resultado->fetch_assoc()) {
+		$clase = ""; // De entrada no tienes clase	
+		if($fila['Tables_in_'.$db] == $_GET['tabla']){ // Pero si el nombre de esta tabla coincide con la tabla cargada
+			$clase = "activo"; // En ese caso tu clase es "activo"
+		}
+		echo '
+			<a href="?tabla='.$fila['Tables_in_'.$db].'" class="'.$clase.'">
+				'.$fila['Tables_in_'.$db].'
+			</a>
+		';
+	}
+?>
+<style>
+	.activo {
+		background: white; /* Ejemplo de estilo para resaltar la tabla activa */
+		font-weight: bold;
+		transform:translateX(20px);
+	}
+</style>
